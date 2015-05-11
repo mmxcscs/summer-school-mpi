cd203ae3307adc719f0f2d76091c7161fd72dd48
Maxime Martinasso <mmx.cscs@gmail.com>
2015-05-11 18:12:48 +0200

    Completed C of part 01

--- practicals/01.MPI_Intro/C/1.hello.c
+++ practicals/01.MPI_Intro/C/1.hello.c
@@ -1,19 +1,11 @@
 /****************************************************************
  *                                                              *
  * This file has been written as a sample solution to an        *
- * exercise in a course given at the High Performance           *
- * Computing Centre Stuttgart (HLRS).                           *
- * The examples are based on the examples in the MPI course of  *
- * the Edinburgh Parallel Computing Centre (EPCC).              *
+ * exercise in a course given at the CSCS Summer School.        *
  * It is made freely available with the understanding that      *
  * every copy of this file must include this header and that    *
- * HLRS and EPCC take no responsibility for the use of the      *
- * enclosed teaching material.                                  *
- *                                                              *
- * Authors: Joel Malard, Alan Simpson,            (EPCC)        *
- *          Rolf Rabenseifner, Traugott Streicher (HLRS)        *
- *                                                              *
- * Contact: rabenseifner@hlrs.de                                *
+ * CSCS take no responsibility for the use of the enclosed      *
+ * teaching material.                                           *
  *                                                              *
  * Purpose: a simple MPI-program printing "hello world!"        *
  *                                                              *
@@ -34,4 +26,5 @@ int main(int argc, char *argv[])
     /* print hello world from each process */
 
     /* finalize MPI */
+    return 0;
 }
--- practicals/01.MPI_Intro/C/2.hello_withrank.c
+++ practicals/01.MPI_Intro/C/2.hello_withrank.c
@@ -1,19 +1,11 @@
 /****************************************************************
  *                                                              *
  * This file has been written as a sample solution to an        *
- * exercise in a course given at the High Performance           *
- * Computing Centre Stuttgart (HLRS).                           *
- * The examples are based on the examples in the MPI course of  *
- * the Edinburgh Parallel Computing Centre (EPCC).              *
+ * exercise in a course given at the CSCS Summer School.        *
  * It is made freely available with the understanding that      *
  * every copy of this file must include this header and that    *
- * HLRS and EPCC take no responsibility for the use of the      *
- * enclosed teaching material.                                  *
- *                                                              *
- * Authors: Joel Malard, Alan Simpson,            (EPCC)        *
- *          Rolf Rabenseifner, Traugott Streicher (HLRS)        *
- *                                                              *
- * Contact: rabenseifner@hlrs.de                                *
+ * CSCS take no responsibility for the use of the enclosed      *
+ * teaching material.                                           *
  *                                                              *
  * Purpose: A program to try MPI_Comm_size and MPI_Comm_rank.   *
  *                                                              *
@@ -31,11 +23,19 @@ int main(int argc, char *argv[])
     MPI_Init(&argc, &argv);
 
     /* Get the rank of each process */
-
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+ 
     /* Get the size of the communicator */
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     /* Write code such that every process writes its rank and the size of the communicator,
      * but only process 0 prints "hello world*/
+    if (my_rank == 0) {
+      printf ("Hello world!\n");
+    }
+
+    printf("I am process %i out of %i.\n", my_rank, size);
 
     MPI_Finalize();
+    return 0;
 }
