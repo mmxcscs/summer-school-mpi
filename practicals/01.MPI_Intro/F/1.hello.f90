92ac0c92dd819796c29dc127fddbff2c3d09dc2f
Maxime Martinasso <mmx.cscs@gmail.com>
2015-05-12 09:13:20 +0200

    Finish F part of 01

--- practicals/01.MPI_Intro/F/1.hello.f90
+++ practicals/01.MPI_Intro/F/1.hello.f90
@@ -3,21 +3,13 @@ PROGRAM hello
 !==============================================================!
 !                                                              !
 ! This file has been written as a sample solution to an        !
-! exercise in a course given at the High Performance           !
-! Computing Centre Stuttgart (HLRS).                           !
-! The examples are based on the examples in the MPI course of  !
-! the Edinburgh Parallel Computing Centre (EPCC).              !
+! exercise in a course given at the CSCS Summer School.        !
 ! It is made freely available with the understanding that      !
 ! every copy of this file must include this header and that    !
-! HLRS and EPCC take no responsibility for the use of the      !
-! enclosed teaching material.                                  !
+! CSCS take no responsibility for the use of the enclosed      !
+! teaching material.                                           !
 !                                                              !
-! Authors: Joel Malard, Alan Simpson,            (EPCC)        !
-!          Rolf Rabenseifner, Traugott Streicher (HLRS)        !
-!                                                              !
-! Contact: rabenseifner@hlrs.de                                !
-!                                                              !
-! Purpose: A simple MPI-program printing "Hello world!"        !
+! Purpose: a simple MPI-program printing "hello world!"        !
 !                                                              !
 ! Contents: F-Source                                           !
 !==============================================================!
