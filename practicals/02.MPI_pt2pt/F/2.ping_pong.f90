39a2b024f1c51c5a0b76348af2d790a0c79224e3
Maxime Martinasso <mmx.cscs@gmail.com>
2015-05-12 11:36:16 +0200

    Complete F part of 02

---
+++ practicals/02.MPI_pt2pt/F/2.ping_pong.f90
@@ -0,0 +1,67 @@
+PROGRAM ping_pong
+
+!==============================================================!
+!                                                              !
+! This file has been written as a sample solution to an        !
+! exercise in a course given at the CSCS Summer School.        !
+! It is made freely available with the understanding that      !
+! every copy of this file must include this header and that    !
+! CSCS take no responsibility for the use of the enclosed      !
+! teaching material.                                           !
+!                                                              !
+! Purpose: A program to try MPI_Ssend and MPI_Recv.            !
+!                                                              !
+! Contents: F-Source                                           !
+!==============================================================!
+
+! This code times the average time it takes for 2 processes to exchange a message
+
+  USE MPI
+  IMPLICIT NONE
+
+  INTEGER PROCESS_A
+  PARAMETER(PROCESS_A=0)
+
+  INTEGER PROCESS_B
+  PARAMETER(PROCESS_B=1)
+
+  INTEGER PING
+  PARAMETER(PING=17) ! message tag
+  
+  INTEGER PONG
+  PARAMETER(PONG=23) ! message tag
+
+  INTEGER NMESSAGES
+  PARAMETER (NMESSAGES=100)
+
+  INTEGER length
+  PARAMETER (length=1)
+ 
+  DOUBLE PRECISION tstart, tstop, time
+  INTEGER status(MPI_STATUS_SIZE)
+   
+  REAL buffer(length)
+
+  INTEGER i
+
+  INTEGER ierror, my_rank, size
+
+  CALL MPI_INIT(ierror)
+
+  CALL MPI_COMM_RANK(MPI_COMM_WORLD, my_rank, ierror)
+
+  tstart = MPI_WTIME()
+
+  ! write a loop of number_of_messages iterations. Within the loop, process A sends a message
+  ! (ping) to process B. After receiving the message, process B sends a message (pong) to process A) 
+
+  tstop = MPI_WTIME()
+
+  IF (my_rank.EQ.PROCESS_A) THEN
+     time = tstop - tstart
+     WRITE(*,*) 'Time for one message:', time/(2*NMESSAGES)*1e6, ' micro seconds'
+  END IF
+
+  CALL MPI_FINALIZE(ierror)
+
+END PROGRAM
