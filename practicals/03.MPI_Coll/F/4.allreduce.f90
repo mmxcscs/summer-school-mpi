c0ee4d98eedeb3bc3878d47c3e8eab3d5b94420c
Maxime Martinasso <mmx.cscs@gmail.com>
2015-07-21 12:14:10 +0200

    Minor changes.

--- practicals/03.MPI_Coll/F/4.allreduce.f90
+++ practicals/03.MPI_Coll/F/4.allreduce.f90
@@ -17,12 +17,12 @@ PROGRAM allreduce
   USE MPI
   IMPLICIT NONE
   INTEGER :: ierror, my_rank
-  INTEGER :: sum   
+  INTEGER :: sum
 
   CALL MPI_Init(ierror)
   CALL MPI_Comm_rank(MPI_COMM_WORLD, my_rank, ierror)
 
-  ! calculate sum of all ranks 
+  ! calculate sum of all ranks
 
   WRITE(*,*) "PE", my_rank, ": Sum =", sum
 
